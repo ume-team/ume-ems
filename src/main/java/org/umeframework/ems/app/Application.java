ea505787a82c4baac0b7406773c81c41aba0ad28
mmayye <mmayye@192.168.1.97>
2017-10-07 15:23:38 +0800

    1

--- src/main/java/org/umeframework/ems/app/Application.java
+++ src/main/java/org/umeframework/ems/app/Application.java
@@ -3,7 +3,6 @@ package org.umeframework.ems.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import org.umeframework.dora.appconfig.DefaultWebControllerConfiguration;
 import org.umeframework.ems.app.appconfig.AppAutoConfiguration;
 
 /**
@@ -14,11 +13,10 @@ import org.umeframework.ems.app.appconfig.AppAutoConfiguration;
 // @ImportResource("classpath:DoraAutoConfiguration.xml")
 @SpringBootApplication(exclude = {
         org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration.class,
-        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class})
+        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class })
 @Import({
         // 启动UME框架的默认配置
-         org.umeframework.dora.appconfig.AutoConfiguration.class,
-        DefaultWebControllerConfiguration.class,
+        org.umeframework.dora.appconfig.AutoConfiguration.class,
         // 启动当前项目的自定义配置
         AppAutoConfiguration.class })
 public class Application {
@@ -29,8 +27,13 @@ public class Application {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
-		SpringApplication.run(Application.class, args);
+		try {
+			SpringApplication.run(Application.class, args);
+		} catch (Throwable e) {
+			if (!e.getClass().getName().startsWith("org.springframework.boot.devtools")) {
+				e.printStackTrace();
+			}
+		}
 
 	}
 }
